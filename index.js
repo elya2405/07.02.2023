4f1bf2c412f692d3e5f39498f6b4f4c4afb84399
kanybekova eleonora <eelya250@gmail.com>
2023-02-08 22:48:05 +0600

    output of elements

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ for (let a = 10; a >= -10; a--) {
   console.log(a);
 }
 
-
 // тема: Массивы (переменная которая хранит несколько значений)
 let example1 = [];
 let example2 = [3, 4, 1, 54, 33];
@@ -36,3 +35,24 @@ let example3 = ["hello", 55, true, null];
 let example4 = ["aibek", "radbek", "elya"];
 
 let example5 = ["issyk-kul", "osh", "chui", "talas", "jalal-abad", "naryn"];
+
+console.log(example3);
+console.log(example3);
+console.log(example3);
+console.log(example4[1]); //отчет идет от нуля, это для того чтобы вывести определенный элемент из массива
+
+example4.push("ajar"); //добавить элемент в массив
+example4.unshift("tina"); //добавить элемент в начало массива
+console.log(example4);
+
+let name1 = example4.pop(); //убирает элемент с конца
+let name2 = example4.shift(); //убирет элемент с начала
+console.log(example4);
+
+example4[0] = "aigerim"; // изменяет элемент в массиве
+console.log(example4);
+console.log(example5.length); //сколько элементов находится в массиве
+
+console.log("hello my mname is " + example4[0]);
+console.log("hello my mname is " + example4[1]);
+console.log("hello my mname is " + example4[2]);
